af60d1517e25746e9e0d4f3da7372d61656c68f0
Mark McDermott <mark@markmcdermott.io>
2022-11-13 00:20:01 -0600

    init

---
+++ app/javascript/application.js
@@ -0,0 +1,11 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+import Vue from 'vue'
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
